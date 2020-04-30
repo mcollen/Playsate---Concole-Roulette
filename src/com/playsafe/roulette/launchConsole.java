ce0089f3c287468361af1f32b8c54468f5bc167a
mcollen <collen@wooddesigns.co.za>
2020-04-30 19:13:12 +0300

    initial commit of the consoul  roulette

--- src/com/playsafe/roulette/launchConsole.java
+++ src/com/playsafe/roulette/launchConsole.java
@@ -1,17 +1,33 @@
 package com.playsafe.roulette;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+
 public class launchConsole implements Runnable{
 
 	@Override
 	public void run() {
 			
 		try {
-			
-			//wait 30seconds before a rerun
-			Thread.sleep(30000);
-		} catch (InterruptedException e) {
+			while (startGame()) {			
+				//wait 30seconds before a rerun
+				Thread.sleep(30000);
+			}
+		} catch (InterruptedException | IOException e) {
 			e.printStackTrace();
 		}
+	}
+	
+	private boolean startGame() throws IOException {		    
+        BufferedReader reader=new BufferedReader(new FileReader("payerList.txt"));
+        String line;
+        while ((line = reader.readLine()) != null) {
+           System.out.println(line);
+        }
+		return false;       
 	}
 
 }
--- src/com/playsafe/roulette/rouletteConsole.java
+++ src/com/playsafe/roulette/rouletteConsole.java
@@ -1,5 +1,9 @@
 package com.playsafe.roulette;
 
-public class rouletteConsole {
+public class rouletteConsole{
 
+	public static void main(String[] args) {
+		launchConsole launch  = new launchConsole();
+		launch.run();
+	}
 }
