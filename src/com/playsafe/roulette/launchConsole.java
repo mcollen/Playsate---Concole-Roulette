6585b6311851444d0c72d76c210bc57f62e68f3a
mcollen <collen@wooddesigns.co.za>
2020-04-30 19:30:41 +0300

    initial commit of the consoul  roulette

--- src/com/playsafe/roulette/launchConsole.java
+++ src/com/playsafe/roulette/launchConsole.java
@@ -1,13 +1,16 @@
 package com.playsafe.roulette;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.Reader;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class launchConsole implements Runnable{
-
+	
+	ArrayList<String> record;
+	HashMap<String,ArrayList<String>> lists = new HashMap<String,ArrayList<String>>();
+	
 	@Override
 	public void run() {
 			
@@ -24,10 +27,23 @@ public class launchConsole implements Runnable{
 	private boolean startGame() throws IOException {		    
         BufferedReader reader=new BufferedReader(new FileReader("payerList.txt"));
         String line;
+        
+        // load all players
         while ((line = reader.readLine()) != null) {
-           System.out.println(line);
+        	
+        	
+        	
+        	//System.out.println(line);
         }
 		return false;       
 	}
 
+	private void loadPlayers(BufferedReader reader) throws IOException {
+		String line;
+		while ((line = reader.readLine()) != null) {
+        	record = new ArrayList<String>();
+        	lists.put(line, record);
+        }
+		
+	}
 }
