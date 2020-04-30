9f3168e249c64765701851fab48319c8651c5ea9
mcollen <collen@wooddesigns.co.za>
2020-04-30 19:55:46 +0300

    functionality addition - roulette

--- src/com/playsafe/roulette/launchConsole.java
+++ src/com/playsafe/roulette/launchConsole.java
@@ -3,6 +3,7 @@ package com.playsafe.roulette;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -10,7 +11,7 @@ public class launchConsole implements Runnable{
 	
 	ArrayList<String> record;
 	HashMap<String,ArrayList<String>> lists = new HashMap<String,ArrayList<String>>();
-	
+		
 	@Override
 	public void run() {
 			
@@ -29,13 +30,16 @@ public class launchConsole implements Runnable{
         String line;
         
         // load all players
-        while ((line = reader.readLine()) != null) {
-        	
-        	
-        	
-        	//System.out.println(line);
-        }
-		return false;       
+        loadPlayers(reader);
+        
+        //Read console input
+        readConsoleInput();
+        
+        //generate rondom number
+        int play = Integer.parseInt(String.valueOf(Math.random()));
+        System.out.println("Winning Number is:    "+play);
+        
+		return true;       
 	}
 
 	private void loadPlayers(BufferedReader reader) throws IOException {
@@ -46,4 +50,21 @@ public class launchConsole implements Runnable{
         }
 		
 	}
+	
+	private void readConsoleInput() throws IOException {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		String line = reader.readLine();
+		String[] bet = line.split(" ");
+		while (bet != null && !bet[0].equals("end")) {
+			record = new ArrayList<String>();
+			if (bet.length > 1) {
+				record.add(bet[1]); //store bet number
+				record.add(bet[2]); //store bet amount
+				lists.get(bet[0]).addAll(record); //keep history
+			} else {
+				System.out.println("Please Enter correct input \n <Player name> [bet] [amount] ... or 'end' to end list");
+			}
+		}
+	}
+	
 }
